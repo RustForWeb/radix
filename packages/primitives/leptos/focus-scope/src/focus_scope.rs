b4fca366d7355a692b0d759196738d0e7c9356be
Daniëlle Huisman <danielle@nixy.software>
2024-06-17 21:42:28 +0200

    Add more of focus-scope

--- packages/primitives/leptos/focus-scope/src/focus_scope.rs
+++ packages/primitives/leptos/focus-scope/src/focus_scope.rs
@@ -1,32 +1,20 @@
 // TODO: remove
 #![allow(unused)]
 
+use std::cell::RefCell;
 use std::ops::Deref;
+use std::rc::Rc;
+use std::sync::atomic::AtomicU64;
+use std::sync::Mutex;
 
 use leptos::{html::AnyElement, *};
+use once_cell::sync::Lazy;
 use radix_leptos_primitive::Primitive;
 use web_sys::{
     wasm_bindgen::{closure::Closure, JsCast},
-    NodeFilter,
+    CustomEvent, FocusEvent, MutationObserver, NodeFilter,
 };
-
-struct FocusScopeValue {
-    pub paused: bool,
-}
-
-impl FocusScopeValue {
-    pub fn new() -> Self {
-        Self { paused: false }
-    }
-
-    pub fn pause(&mut self) {
-        self.paused = true;
-    }
-
-    pub fn resume(&mut self) {
-        self.paused = false;
-    }
-}
+use web_sys::{MutationObserverInit, MutationRecord};
 
 #[component]
 pub fn FocusScope(
@@ -40,14 +28,178 @@ pub fn FocusScope(
     // let r#loop = move || r#loop.get().unwrap_or(false);
     let trapped = move || trapped.get().unwrap_or(false);
 
-    // let container_ref = create_node_ref::<AnyElement>();
-    // let last_focused_element = create_signal::<Option<HtmlElement<AnyElement>>>(None);
+    let container_ref = create_node_ref::<AnyElement>();
+    let last_focused_element = create_rw_signal::<Option<web_sys::HtmlElement>>(None);
+    let focus_scope = create_rw_signal(FocusScopeAPI::new());
 
-    // let focus_scope = create_rw_signal(FocusScopeValue::new());
+    let handle_focus_in: Rc<Closure<dyn Fn(FocusEvent)>> =
+        Rc::new(Closure::new(move |event: FocusEvent| {
+            let focus_scope = focus_scope.get_untracked();
+            if focus_scope.paused {
+                return;
+            }
+
+            if let Some(container) = container_ref.get_untracked() {
+                let target = event
+                    .target()
+                    .map(|target| target.unchecked_into::<web_sys::HtmlElement>());
+
+                if container.contains(target.as_ref().map(|e| e.unchecked_ref())) {
+                    last_focused_element.set(target);
+                } else {
+                    focus(
+                        last_focused_element.get_untracked(),
+                        Some(FocusOptions { select: true }),
+                    );
+                }
+            }
+        }));
+
+    let handle_focus_out: Rc<Closure<dyn Fn(FocusEvent)>> =
+        Rc::new(Closure::new(move |event: FocusEvent| {
+            let focus_scope = focus_scope.get_untracked();
+            if focus_scope.paused {
+                return;
+            }
+
+            if let Some(container) = container_ref.get_untracked() {
+                let related_target = event
+                    .related_target()
+                    .map(|target| target.unchecked_into::<web_sys::HtmlElement>());
+
+                // A `focusout` event with a `None` `related_target` will happen in at least two cases:
+                //
+                // 1. When the user switches app/tabs/windows/the browser itself loses focus.
+                // 2. In Google Chrome, when the focused element is removed from the DOM.
+                //
+                // We let the browser do its thing here because:
+                //
+                // 1. The browser already keeps a memory of what's focused for when the page gets refocused.
+                // 2. In Google Chrome, if we try to focus the deleted focused element (as per below), it
+                //    throws the CPU to 100%, so we avoid doing anything for this reason here too.
+                if related_target.is_none() {
+                    return;
+                }
+
+                // If the focus has moved to an actual legitimate element (`related_target != None`)
+                // that is outside the container, we move focus to the last valid focused element inside.
+                if container.contains(related_target.as_ref().map(|e| e.unchecked_ref())) {
+                    focus(
+                        last_focused_element.get_untracked(),
+                        Some(FocusOptions { select: true }),
+                    );
+                }
+            }
+        }));
+
+    let mutation_observer: Rc<RefCell<Option<MutationObserver>>> = Rc::new(RefCell::new(None));
+
+    let cleanup_handle_focus_in = handle_focus_in.clone();
+    let cleanup_handle_focus_out = handle_focus_out.clone();
+    let cleanup_mutation_observer = mutation_observer.clone();
 
     create_effect(move |_| {
         if trapped() {
-            // TODO
+            document()
+                .add_event_listener_with_callback(
+                    "focusin",
+                    (*handle_focus_in).as_ref().unchecked_ref(),
+                )
+                .expect("Focus in event listener should be added.");
+            document()
+                .add_event_listener_with_callback(
+                    "focusout",
+                    (*handle_focus_out).as_ref().unchecked_ref(),
+                )
+                .expect("Focus out event listener should be added.");
+        }
+    });
+
+    create_effect(move |_| {
+        if trapped() {
+            if let Some(container) = container_ref.get() {
+                if let Some(mutation_observer) = mutation_observer.take() {
+                    mutation_observer.disconnect();
+                }
+
+                // When the focused element gets removed from the DOM, browsers move focus
+                // back to the document.body. In this case, we move focus to the container
+                // to keep focus trapped correctly.
+                let handle_mutations: Closure<dyn Fn(Vec<MutationRecord>)> =
+                    Closure::new(move |mutations: Vec<MutationRecord>| {
+                        let focused_element = document()
+                            .active_element()
+                            .map(|element| element.unchecked_into::<web_sys::HtmlElement>());
+                        if focused_element != document().body() {
+                            return;
+                        }
+
+                        for mutation in mutations {
+                            if mutation.removed_nodes().length() > 0 {
+                                focus(container_ref.get_untracked().as_deref().cloned(), None);
+                            }
+                        }
+                    });
+
+                mutation_observer.replace(Some(
+                    MutationObserver::new(handle_mutations.into_js_value().unchecked_ref())
+                        .expect("Mutation observer should be created."),
+                ));
+
+                mutation_observer
+                    .borrow()
+                    .as_ref()
+                    .expect("Mutation observer should exist.")
+                    .observe_with_options(
+                        &container,
+                        MutationObserverInit::new().child_list(true).subtree(true),
+                    )
+                    .expect("Mutation observer should observe target.");
+            }
+        }
+    });
+
+    create_effect(move |_| {
+        if let Some(container) = container_ref.get() {
+            {
+                let mut focus_scope_stack = FOCUS_SCOPE_STACK
+                    .lock()
+                    .expect("Focus scope stack mutex should lock.");
+                focus_scope_stack.add(focus_scope.get());
+            }
+
+            let previously_focused_element = document()
+                .active_element()
+                .map(|element| element.unchecked_into::<web_sys::HtmlElement>());
+            let has_focused_candidate = container.contains(
+                previously_focused_element
+                    .as_ref()
+                    .map(|element| element.unchecked_ref()),
+            );
+
+            if !has_focused_candidate {
+                // let mount_event = CustomEvent::new_with_event_init_dict();
+                // TODO
+            }
+        }
+    });
+
+    on_cleanup(move || {
+        document()
+            .remove_event_listener_with_callback(
+                "focusin",
+                (*cleanup_handle_focus_in).as_ref().unchecked_ref(),
+            )
+            .expect("Focus in event listener should be removed.");
+        document()
+            .remove_event_listener_with_callback(
+                "focusout",
+                (*cleanup_handle_focus_out).as_ref().unchecked_ref(),
+            )
+            .expect("Focus out event listener should be removed.");
+
+        if let Some(mutation_observer) = cleanup_mutation_observer.take() {
+            mutation_observer.disconnect();
         }
     });
 
@@ -242,5 +394,131 @@ fn focus(element: Option<web_sys::HtmlElement>, options: Option<FocusOptions>) {
                 .unchecked_into::<web_sys::HtmlInputElement>()
                 .select();
         }
+    }
+}
+
+static FOCUS_SCOPE_STACK: Lazy<Mutex<FocusScopeStack>> =
+    Lazy::new(|| Mutex::new(FocusScopeStack::new()));
+
+#[derive(Clone, Debug)]
+struct FocusScopeAPI {
+    id: u64,
+    paused: bool,
+}
+
+impl FocusScopeAPI {
+    pub fn new() -> Self {
+        static COUNTER: AtomicU64 = AtomicU64::new(1);
+        Self {
+            id: COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+            paused: false,
+        }
+    }
+
+    pub fn pause(&mut self) {
+        self.paused = true;
+    }
+
+    pub fn resume(&mut self) {
+        self.paused = false;
+    }
+}
+
+impl PartialEq for FocusScopeAPI {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+/// A stack of focus scopes, with the active one at the top.
+#[derive(Clone, Debug, PartialEq)]
+struct FocusScopeStack {
+    stack: Vec<FocusScopeAPI>,
+}
+
+impl FocusScopeStack {
+    pub fn new() -> Self {
+        Self { stack: vec![] }
+    }
+
+    pub fn add(&mut self, focus_scope: FocusScopeAPI) {
+        // Pause the currently active focus scope (at the top of the stack).
+        if let Some(active_focus_scope) = self.stack.first_mut() {
+            if focus_scope != *active_focus_scope {
+                active_focus_scope.pause();
+            }
+        }
+
+        // Remove in case it already exists (because we'll re-add it at the top of the stack).
+        self.remove_without_resume(&focus_scope);
+        self.stack.insert(0, focus_scope);
+    }
+
+    pub fn remove(&mut self, focus_scope: &FocusScopeAPI) {
+        self.remove_without_resume(focus_scope);
+
+        if let Some(active_focus_scope) = self.stack.first_mut() {
+            active_focus_scope.resume();
+        }
+    }
+
+    fn remove_without_resume(&mut self, focus_scope: &FocusScopeAPI) {
+        let index = self.stack.iter().position(|f| f == focus_scope);
+
+        if let Some(index) = index {
+            self.stack.remove(index);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_focus_scope_stack() {
+        let mut stack = FocusScopeStack::new();
+
+        let a = FocusScopeAPI::new();
+        let b = FocusScopeAPI::new();
+        let c = FocusScopeAPI::new();
+
+        stack.add(a.clone());
+        assert_eq!(vec![a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+
+        stack.add(b.clone());
+        assert_eq!(vec![b.clone(), a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+        assert!(stack.stack[1].paused);
+
+        stack.add(c.clone());
+        assert_eq!(vec![c.clone(), b.clone(), a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+        assert!(stack.stack[1].paused);
+        assert!(stack.stack[2].paused);
+
+        stack.add(b.clone());
+        assert_eq!(vec![b.clone(), c.clone(), a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+        assert!(stack.stack[1].paused);
+        assert!(stack.stack[2].paused);
+
+        stack.remove(&c);
+        assert_eq!(vec![b.clone(), a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+        assert!(stack.stack[1].paused);
+
+        stack.remove(&c);
+        assert_eq!(vec![b.clone(), a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+        assert!(stack.stack[1].paused);
+
+        stack.remove(&b);
+        assert_eq!(vec![a.clone()], stack.stack);
+        assert!(!stack.stack[0].paused);
+
+        stack.remove(&a);
+        assert!(stack.stack.is_empty());
     }
 }
