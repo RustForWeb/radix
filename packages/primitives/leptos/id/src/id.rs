00985c59781fe079673cac752b937594dfb42bb7
Daniëlle Huisman <daniel@huisman.me>
2025-01-03 17:02:26 +0100

    Update ID to Leptos v0.7 (#400)

--- packages/primitives/leptos/id/src/id.rs
+++ packages/primitives/leptos/id/src/id.rs
@@ -1,11 +1,11 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use leptos::*;
+use leptos::prelude::*;
 
 static COUNT: AtomicUsize = AtomicUsize::new(0);
 
 pub fn use_id(deterministic_id: Option<String>) -> ReadSignal<String> {
-    let (id, _) = create_signal(
+    let (id, _) = signal(
         deterministic_id
             .unwrap_or_else(|| format!("radix-{}", COUNT.fetch_add(1, Ordering::Relaxed))),
     );
