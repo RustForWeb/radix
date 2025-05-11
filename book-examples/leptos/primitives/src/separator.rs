84827973b5f1894db46ebf55f974666903f72103
Geoffrey Garrett <26066340+geoffreygarrett@users.noreply.github.com>
2025-05-11 13:19:01 +0200

    chore(separator): update Separator to Leptos 0.7 (#439)
    * Update Separator to Leptos 0.7
    
    * Cleanup
    
    ---------
    
    Co-authored-by: geoffreygarrett <geoffrey@apollo.xyz>
    Co-authored-by: Daniëlle Huisman <danielle@huisman.me>

--- book-examples/leptos/primitives/src/separator.rs
+++ book-examples/leptos/primitives/src/separator.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::prelude::*;
 use radix_leptos_separator::*;
 
 #[component]
--- packages/primitives/leptos/separator/src/separator.rs
+++ packages/primitives/leptos/separator/src/separator.rs
@@ -1,6 +1,8 @@
 use std::fmt::{Display, Formatter};
 
-use leptos::{html::AnyElement, *};
+use leptos::{html, prelude::*};
+use leptos_node_ref::AnyNodeRef;
+use radix_leptos_primitive::Primitive;
 
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
 pub enum Orientation {
@@ -22,61 +24,36 @@ impl Display for Orientation {
     }
 }
 
-impl IntoAttribute for Orientation {
-    fn into_attribute(self) -> Attribute {
-        Attribute::String(self.to_string().into())
-    }
-
-    fn into_attribute_boxed(self: Box<Self>) -> Attribute {
-        self.into_attribute()
-    }
-}
-
 #[component]
 pub fn Separator(
     #[prop(into, optional)] orientation: MaybeProp<Orientation>,
     #[prop(into, optional)] decorative: MaybeProp<bool>,
     #[prop(into, optional)] as_child: MaybeProp<bool>,
-    #[prop(optional)] node_ref: NodeRef<AnyElement>,
-    #[prop(attrs)] attrs: Vec<(&'static str, Attribute)>,
+    #[prop(into, optional)] node_ref: AnyNodeRef,
     #[prop(optional)] children: Option<ChildrenFn>,
 ) -> impl IntoView {
     let children = StoredValue::new(children);
 
     let orientation = Signal::derive(move || orientation.get().unwrap_or_default());
-    let aria_orientation = Signal::derive(move || match orientation.get() {
-        Orientation::Horizontal => None,
-        Orientation::Vertical => Some("vertical".to_string()),
-    });
     let decorative = Signal::derive(move || decorative.get().unwrap_or_default());
-
-    let mut attrs = attrs.clone();
-    attrs.extend([
-        ("data-orientation", orientation.into_attribute()),
-        (
-            "aria-orientation",
-            (match decorative.get() {
-                true => aria_orientation.get(),
-                false => None,
-            })
-            .into_attribute(),
-        ),
-        (
-            "role",
-            (match decorative.get() {
-                true => "none",
-                false => "separator",
-            })
-            .into_attribute(),
-        ),
-    ]);
 
     view! {
         <Primitive
             element=html::div
             as_child=as_child
             node_ref=node_ref
-            attrs=attrs
+            attr:data-orientation=move || orientation.get().to_string()
+            attr:aria-orientation=move || {
+                if decorative.get() {
+                    None
+                } else {
+                    match orientation.get() {
+                        Orientation::Vertical => Some("vertical".to_owned()),
+                        Orientation::Horizontal => None,
+                    }
+                }
+            }
+            attr:role=move || if decorative.get() { "none" } else { "separator" }
         >
             {children.with_value(|children| children.as_ref().map(|children| children()))}
         </Primitive>
