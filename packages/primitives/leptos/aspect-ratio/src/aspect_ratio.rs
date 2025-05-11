1f22dd9f032dde404fc54afa76d0fe5d33deafd3
Geoffrey Garrett <26066340+geoffreygarrett@users.noreply.github.com>
2025-05-11 19:34:03 +0200

    chore(aspect-ratio): update to Leptos 0.7 (#419)
    * Update Aspect Ratio to Leptos 0.7
    
    * Cleanup
    
    ---------
    
    Co-authored-by: geoffreygarrett <geoffrey@apollo.xyz>
    Co-authored-by: Daniëlle Huisman <danielle@huisman.me>

--- packages/primitives/leptos/aspect-ratio/src/aspect_ratio.rs
+++ packages/primitives/leptos/aspect-ratio/src/aspect_ratio.rs
@@ -1,80 +1,41 @@
-use leptos::prelude::*;
-use leptos_style::Style;
-
-pub struct UseAspectRatioProps {
-    style: Style,
-}
-
-pub struct UseAspectRatioAttrs {
-    style: Style,
-}
-
-pub fn use_aspect_ratio(props: UseAspectRatioProps) -> UseAspectRatioAttrs {
-    UseAspectRatioAttrs {
-        style: props.style.with_defaults([
-            // Ensures children expand in ratio.
-            ("position", "absolute"),
-            ("top", "0px"),
-            ("right", "0px"),
-            ("bottom", "0px"),
-            ("left", "0px"),
-        ]),
-    }
-}
-
-#[component]
-pub fn BaseAspectRatio(
-    #[prop(into, optional)] ratio: MaybeProp<f64>,
-    #[prop(optional)] children: Option<Children>,
-) -> impl IntoView {
-    let ratio = Signal::derive(move || ratio.get().unwrap_or(1.0));
-
-    view! {
-        <div
-            // Ensures inner element is contained.
-            style:position="relative"
-            // Ensures padding bottom trick maths works.
-            style:width="100%"
-            style:padding-bottom=move || format!("{}%", 100.0 / ratio.get())
-            data-radix-aspect-ratio-wrapper=""
-        >
-            {children.map(|children| children())}
-        </div>
-    }
-}
+use leptos::{attribute_interceptor::AttributeInterceptor, html, prelude::*};
+use leptos_node_ref::AnyNodeRef;
+use radix_leptos_primitive::Primitive;
 
 #[component]
 pub fn AspectRatio(
-    #[prop(into, optional)] ratio: MaybeProp<f64>,
-    #[prop(into, optional)] style: Style,
-    #[prop(optional)] children: Option<Children>,
+    #[prop(into, optional, default = 1.0.into())] ratio: Signal<f64>,
+    #[prop(into, optional)] as_child: MaybeProp<bool>,
+    #[prop(optional)] node_ref: AnyNodeRef,
+    children: TypedChildrenFn<impl IntoView + 'static>,
 ) -> impl IntoView {
-    let attrs = use_aspect_ratio(UseAspectRatioProps { style });
+    let children = StoredValue::new(children.into_inner());
 
     view! {
-        <BaseAspectRatio ratio=ratio>
-            <div style={attrs.style}>
-                {children.map(|children| children())}
+        <AttributeInterceptor let:attrs>
+            <div
+                // Ensures inner element is contained
+                style:position="relative"
+                // Ensures padding bottom trick maths works
+                style:width="100%"
+                style:padding-bottom=move || format!("{}%", 100.0 / ratio.get())
+                data-radix-aspect-ratio-wrapper=""
+            >
+                <Primitive
+                    element=html::div
+                    as_child=as_child
+                    node_ref=node_ref
+                    // Ensures children expand in ratio
+                    style:position="absolute"
+                    style:top="0px"
+                    style:right="0px"
+                    style:bottom="0px"
+                    style:left="0px"
+                    {..attrs}
+                >
+                    {children.with_value(|children| children())}
+                </Primitive>
             </div>
-        </BaseAspectRatio>
-    }
-}
-
-#[component]
-pub fn AspectRatioAsChild<R, RV>(
-    #[prop(into, optional)] ratio: MaybeProp<f64>,
-    #[prop(into, optional)] style: Style,
-    render: R,
-) -> impl IntoView
-where
-    R: Fn(UseAspectRatioAttrs) -> RV + Send + 'static,
-    RV: IntoView + 'static,
-{
-    let attrs = use_aspect_ratio(UseAspectRatioProps { style });
-
-    view! {
-        <BaseAspectRatio ratio=ratio>
-            {render(attrs)}
-        </BaseAspectRatio>
+        </AttributeInterceptor>
     }
 }
