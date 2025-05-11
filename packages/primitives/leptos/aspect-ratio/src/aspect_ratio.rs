585175bf9721783faf81324f048c1571c121748e
Daniëlle Huisman <danielle@huisman.me>
2025-05-11 19:28:28 +0200

    Cleanup

--- packages/primitives/leptos/aspect-ratio/src/aspect_ratio.rs
+++ packages/primitives/leptos/aspect-ratio/src/aspect_ratio.rs
@@ -1,72 +1,37 @@
-use leptos::{prelude::*, html, attribute_interceptor::AttributeInterceptor};
-use radix_leptos_primitive::Primitive;
+use leptos::{attribute_interceptor::AttributeInterceptor, html, prelude::*};
 use leptos_node_ref::AnyNodeRef;
-
-const DEFAULT_RATIO: f64 = 1.0;
-
-/* -------------------------------------------------------------------------------------------------
- * AspectRatio
- * -----------------------------------------------------------------------------------------------*/
-
-const NAME: &'static str = "AspectRatio";
+use radix_leptos_primitive::Primitive;
 
 #[component]
-#[allow(non_snake_case)]
 pub fn AspectRatio(
-    /// Children passed to the AspectRatio component
+    #[prop(into, optional, default = 1.0.into())] ratio: Signal<f64>,
+    #[prop(into, optional)] as_child: MaybeProp<bool>,
+    #[prop(optional)] node_ref: AnyNodeRef,
     children: TypedChildrenFn<impl IntoView + 'static>,
-    /// Change the default rendered element for the one passed as a child
-    #[prop(into, optional, default = false.into())]
-    as_child: MaybeProp<bool>,
-    /// The desired ratio when rendering the content (e.g., 16/9). Defaults to 1.0 if not specified.
-    #[prop(into, optional, default = DEFAULT_RATIO.into())]
-    ratio: MaybeProp<f64>,
-    /// Reference to the underlying DOM node
-    #[prop(into, optional)]
-    node_ref: AnyNodeRef,
 ) -> impl IntoView {
-    // attribute interceptor incurs this requirement
     let children = StoredValue::new(children.into_inner());
 
-    // calculates the percent-based padding for the aspect ratio
-    let padding_bottom = Signal::derive(move || {
-        100.0
-            / ratio
-            .get()
-            .unwrap_or(DEFAULT_RATIO)
-            .clamp(f64::EPSILON, f64::MAX)
-    });
-
-    #[cfg(debug_assertions)]
-    Effect::new(move |_| {
-        leptos::logging::log!("[{NAME}] ratio: {:?}", ratio.get());
-        leptos::logging::log!("[{NAME}] as_child: {:?}", as_child.get());
-        leptos::logging::log!("[{NAME}] node_ref: {:?}", node_ref.get());
-        leptos::logging::log!("[{NAME}] padding_bottom: {:?}", padding_bottom.get());
-    });
-
     view! {
-        // replicate the radix react spread props
         <AttributeInterceptor let:attrs>
-            // ensures inner element is contained
             <div
+                // Ensures inner element is contained
                 style:position="relative"
-                // ensures padding bottom trick works
+                // Ensures padding bottom trick maths works
                 style:width="100%"
-                style:padding-bottom=move || format!("{}%", padding_bottom.get())
+                style:padding-bottom=move || format!("{}%", 100.0 / ratio.get())
                 data-radix-aspect-ratio-wrapper=""
             >
                 <Primitive
-                    // ensures children expand to fill the ratio
-                    element={html::div}
-                    as_child={as_child}
-                    node_ref={node_ref}
-                    {..attrs}
+                    element=html::div
+                    as_child=as_child
+                    node_ref=node_ref
+                    // Ensures children expand in ratio
                     style:position="absolute"
-                    style:top="0"
-                    style:right="0"
-                    style:bottom="0"
-                    style:left="0"
+                    style:top="0px"
+                    style:right="0px"
+                    style:bottom="0px"
+                    style:left="0px"
+                    {..attrs}
                 >
                     {children.with_value(|children| children())}
                 </Primitive>
@@ -74,7 +39,3 @@ pub fn AspectRatio(
         </AttributeInterceptor>
     }
 }
-
-/* -----------------------------------------------------------------------------------------------*/
-
-pub use AspectRatio as Root;
