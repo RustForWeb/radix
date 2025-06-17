01ff2d1b4080cf83fcb8b3de24adf6fd3d934de4
Omar Mohamed <mohamed.omar67492@gmail.com>
2025-06-17 11:13:38 +0300

    refactor: new implementation using js_sys
    Signed-off-by: Omar Mohamed <mohamed.omar67492@gmail.com>

--- packages/primitives/core/rect/src/observe_element_rect.rs
+++ packages/primitives/core/rect/src/observe_element_rect.rs
@@ -1,38 +1,51 @@
-use std::sync::{Arc, LazyLock};
+use std::sync::LazyLock;
 
-use dashmap::DashMap;
 use send_wrapper::SendWrapper;
+use wasm_bindgen::prelude::*;
+use web_sys::js_sys::Map;
 use web_sys::{
-    DomRect, Element, ResizeObserver, ResizeObserverBoxOptions, ResizeObserverEntry,
-    ResizeObserverOptions,
-    wasm_bindgen::{JsCast, closure::Closure},
+    DomRect, Element, ResizeObserver, ResizeObserverEntry, ResizeObserverOptions, js_sys,
 };
-
-type Callback = Arc<dyn Fn(DomRect) + Send + Sync>;
+use web_sys::{ResizeObserverBoxOptions, wasm_bindgen};
 
 pub type UnobserveCallback = Box<dyn Fn() + Send + Sync>;
 
-struct ElementData {
-    callbacks: Vec<Option<Callback>>,
-}
-
-static OBSERVED_ELEMENTS: LazyLock<DashMap<String, ElementData>> = LazyLock::new(DashMap::new);
+static OBSERVED_ELEMENTS: LazyLock<SendWrapper<Map>> =
+    LazyLock::new(|| SendWrapper::new(Map::new()));
 
 static RESIZE_OBSERVER: LazyLock<SendWrapper<ResizeObserver>> = LazyLock::new(|| {
     let callback: Closure<dyn Fn(Vec<ResizeObserverEntry>)> =
         Closure::new(|entries: Vec<ResizeObserverEntry>| {
             for entry in entries {
                 let target = entry.target();
-                let id = target.id();
 
-                let callbacks = if let Some(observed_data) = OBSERVED_ELEMENTS.get(&id) {
-                    Some(observed_data.callbacks.clone())
-                } else {
-                    None
-                };
+                let observed_data = OBSERVED_ELEMENTS.get(&target);
 
-                for callback in callbacks.iter().flatten().flatten() {
-                    callback(target.get_bounding_client_rect().clone());
+                if observed_data == JsValue::UNDEFINED {
+                    let observed_data = OBSERVED_ELEMENTS
+                        .get(&target)
+                        .dyn_into::<js_sys::Object>()
+                        .expect("Object should be of type ObservedData");
+
+                    js_sys::Reflect::set(
+                        &observed_data,
+                        &"rect".into(),
+                        &target.get_bounding_client_rect(),
+                    )
+                    .expect("Should be able to set rect");
+
+                    let callbacks = js_sys::Reflect::get(&observed_data, &"callbacks".into())
+                        .expect("Object should have callbacks array")
+                        .dyn_into::<js_sys::Array>()
+                        .expect("Object should be of type array");
+
+                    callbacks.for_each(&mut |obj, _, _| {
+                        if let Ok(callback) = obj.dyn_into::<js_sys::Function>() {
+                            callback
+                                .call1(&JsValue::NULL, &target.get_bounding_client_rect())
+                                .expect("callback should be called");
+                        }
+                    });
                 }
             }
         });
@@ -43,81 +56,86 @@ static RESIZE_OBSERVER: LazyLock<SendWrapper<ResizeObserver>> = LazyLock::new(||
     )
 });
 
-/// uses `ResizeObserver` to observe an element and calls all the registered callbacks when element's
-/// size changes
-///
-/// # Panics
-///
-/// Panics if failed to acquire locks
-#[allow(clippy::significant_drop_tightening)]
 pub fn observe_element_rect<C>(element_to_observe: &Element, callback: C) -> UnobserveCallback
 where
     C: Fn(DomRect) + 'static + Send + Sync,
 {
-    let callback: Arc<dyn Fn(DomRect) + 'static + Send + Sync> = Arc::new(callback);
-    let mut observed = true;
-    let mut callback_idx = 0;
-    // TODO: find a better way to identify elements
-    let id = {
-        let id = element_to_observe.id();
-        if id.is_empty() {
-            let uuid = uuid::Uuid::new_v4().to_string();
-            element_to_observe.set_id(&uuid);
+    let observed_data = OBSERVED_ELEMENTS.get(element_to_observe);
+    let callback: Closure<dyn Fn(DomRect)> = Closure::new(move |rect: DomRect| callback(rect));
+    let callback = callback.into_js_value();
 
-            uuid
-        } else {
-            id
-        }
-    };
+    if observed_data == JsValue::UNDEFINED {
+        let obj = js_sys::Object::new();
+        let callbacks = js_sys::Array::new();
+        callbacks.push(callback.unchecked_ref());
 
-    {
-        let mut observed_data = OBSERVED_ELEMENTS.entry(id.clone()).or_insert_with(|| {
-            observed = false;
+        js_sys::Reflect::set(&obj, &"rect".into(), &JsValue::NULL)
+            .expect("Should be able to set rect");
+        js_sys::Reflect::set(&obj, &"callbacks".into(), &callbacks.into())
+            .expect("Should be able to set callbacks");
 
-            ElementData {
-                callbacks: Vec::with_capacity(1),
-            }
-        });
+        OBSERVED_ELEMENTS.set(element_to_observe, &obj);
 
-        callback_idx = observed_data.callbacks.len();
-        observed_data.callbacks.push(Some(Arc::clone(&callback)));
-    }
-
-    if !observed {
         let options = ResizeObserverOptions::new();
         options.set_box(ResizeObserverBoxOptions::BorderBox);
 
         RESIZE_OBSERVER.observe_with_options(element_to_observe, &options);
+    } else {
+        let observed_data = observed_data
+            .dyn_into::<js_sys::Object>()
+            .expect("observed data type should be object");
+
+        let callbacks = js_sys::Reflect::get(&observed_data, &"callbacks".into())
+            .expect("Object should have callbacks array")
+            .dyn_into::<js_sys::Array>()
+            .expect("Object should be of type array");
+
+        callbacks.push(callback.unchecked_ref());
+
+        if let Ok(callback) = callbacks
+            .get(callbacks.length() - 1)
+            .dyn_into::<js_sys::Function>()
+        {
+            callback
+                .call1(
+                    &JsValue::NULL,
+                    &js_sys::Reflect::get(&observed_data, &"rect".into())
+                        .expect("ObservedData should have rect")
+                        .dyn_into::<DomRect>()
+                        .expect("rect should be of type DOMRect"),
+                )
+                .expect("callback should be called");
+        }
     }
 
     let wrapped_element = SendWrapper::new(element_to_observe.clone());
+    let wrapped_closure = SendWrapper::new(callback);
     Box::new(move || {
         let element = &*wrapped_element;
+        let callback = &*wrapped_closure;
+        let observed_data = OBSERVED_ELEMENTS.get(element);
 
-        let remove = {
-            let observed_data = OBSERVED_ELEMENTS.get_mut(&id);
+        if observed_data == JsValue::UNDEFINED {
+            return;
+        }
 
-            if let Some(mut observed_data) = observed_data {
-                observed_data.callbacks[callback_idx] = None;
+        let observed_data = observed_data
+            .dyn_into::<js_sys::Object>()
+            .expect("observed data type should be object");
 
-                if observed_data
-                    .callbacks
-                    .iter()
-                    .any(std::option::Option::is_some)
-                {
-                    false
-                } else {
-                    RESIZE_OBSERVER.unobserve(element);
+        let callbacks = js_sys::Reflect::get(&observed_data, &"callbacks".into())
+            .expect("Object should have callbacks array")
+            .dyn_into::<js_sys::Array>()
+            .expect("Object should be of type array");
 
-                    true
-                }
-            } else {
-                false
-            }
-        };
+        let index = callbacks.index_of(callback.unchecked_ref(), 0);
 
-        if remove {
-            OBSERVED_ELEMENTS.remove(&id);
+        if index > -1 {
+            callbacks.splice(index as u32, 1, &JsValue::NULL);
+        }
+
+        if callbacks.length() == 0 {
+            OBSERVED_ELEMENTS.delete(element);
         }
     })
 }
