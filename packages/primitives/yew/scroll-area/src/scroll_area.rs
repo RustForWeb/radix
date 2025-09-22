bd99180f0eab8b70b9296c2dee73768825ac2d96
Daniëlle Huisman <danielle@huisman.me>
2025-09-22 11:05:51 +0200

    fix: ignore dead code warnings

--- packages/primitives/yew/scroll-area/src/scroll_area.rs
+++ packages/primitives/yew/scroll-area/src/scroll_area.rs
@@ -1,3 +1,6 @@
+// TODO: Remove
+#![allow(dead_code)]
+
 use radix_yew_direction::{Direction, use_direction};
 use yew::prelude::*;
 use yew_struct_component::{Attributes, StructComponent, struct_component};
--- stories/yew/src/primitives/scroll_area.rs
+++ stories/yew/src/primitives/scroll_area.rs
@@ -1,3 +1,6 @@
+// TODO: Remove
+#![allow(dead_code)]
+
 use radix_yew_scroll_area::*;
 use tailwind_fuse::*;
 use yew::prelude::*;
